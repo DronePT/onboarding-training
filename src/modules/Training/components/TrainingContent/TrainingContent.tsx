24eafa235d7679d5b7bec4aa2d2d9d5c94228c8f
André Alves <andre.alves@inspireit.pt>
2022-10-18 10:41:05 +0100

    :sparkles: Add code highlight theme selector

--- src/modules/Training/components/TrainingContent/TrainingContent.tsx
+++ src/modules/Training/components/TrainingContent/TrainingContent.tsx
@@ -4,26 +4,63 @@ import remarkGfm from 'remark-gfm';
 import './TrainingContent.scss';
 
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { funky } from 'react-syntax-highlighter/dist/esm/styles/prism';
+// import {  as theme } from 'react-syntax-highlighter/dist/esm/styles/prism';
+// import { coldarkDark as theme } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { useState } from 'react';
+import * as themes from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 interface TrainingContentProps {
   content: string;
 }
 
 export const TrainingContent = (props: TrainingContentProps): JSX.Element => {
+  const [themeName, setThemeName] = useState('materialDark');
+
+  const themesList = Object.keys(themes);
+  const selectedTheme = (themes as any)[themeName];
+
   return (
     <div className='h-full pb-4 TrainingContent markdown'>
+      <div className='flex justify-end items-center p-4 gap-2 border-b border-b-blue-100'>
+        <span className='text-gray-400 text-sm uppercase leading-10'>
+          Code highlight theme:{' '}
+        </span>
+        <select
+          className='border border-blue-500 rounded-lg py-1 px-2'
+          value={themeName}
+          onChange={(e) => setThemeName(e.target.value)}
+        >
+          {themesList.map((theme) => (
+            <option key={theme} value={theme}>
+              {theme}
+            </option>
+          ))}
+        </select>
+      </div>
       <ReactMarkdown
         children={props.content}
         remarkPlugins={[remarkGfm]}
         components={{
           img: ({ node, ...props }) => {
-            const re = /(.*)(=([0-9]*)x([0-9]*))/i;
             const srcLink = String(node?.properties?.src || '');
+            const re = /(.*)(=([0-9]*)x([0-9]*))/i;
 
             const exec = re.exec(srcLink) || [, srcLink, , , ,];
 
             const [, link, , width, height] = exec;
+
+            if (srcLink.includes('=inline')) {
+              return (
+                <img
+                  {...props}
+                  src={link?.replaceAll('=inline', '')}
+                  alt={node.properties?.alt as string}
+                  width={width || undefined}
+                  height={height || undefined}
+                  className='inline'
+                />
+              );
+            }
 
             return (
               <span className='flex items-center justify-center my-8'>
@@ -45,7 +82,7 @@ export const TrainingContent = (props: TrainingContentProps): JSX.Element => {
             return !inline && match ? (
               <SyntaxHighlighter
                 children={String(children).replace(/\n$/, '')}
-                style={funky as any}
+                style={selectedTheme}
                 language={match[1]}
                 PreTag='div'
                 showLineNumbers
